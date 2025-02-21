cbee182cce451d63f7e48db5015d61170787967c
barath <82313227+Barath-2001@users.noreply.github.com>
2025-02-21 16:24:43 +0530

    Update home.py

--- home.py
+++ home.py
@@ -6,20 +6,24 @@ continents_countries = {
     'Europe': ['France', 'Germany', 'Italy'],
 }
 
-st.title('Continent and Country Selection')
+@st.fragment
+def continent_dropdown():
+    return st.selectbox('Select Continent', 
+                        options=['Select a continent'] + list(continents_countries.keys()),
+                        key='continent_key')
 
-selected_continent = st.selectbox('Select Continent', 
-                                  options=['Select a continent'] + list(continents_countries.keys()),
-                                  key='continent_key')
+@st.fragment
+def country_dropdown(continent):
+    if continent == 'Select a continent':
+        countries = ['Select a country']
+    else:
+        countries = continents_countries.get(continent, [])
+    return st.selectbox('Select Country', 
+                        options=['Select a country'] + countries,
+                        key='country_key')
 
-if selected_continent == 'Select a continent':
-    countries = ['Select a country']
-else:
-    countries = continents_countries.get(selected_continent, [])
-
-selected_country = st.selectbox('Select Country', 
-                                options=['Select a country'] + countries,
-                                key='country_key')
+selected_continent = continent_dropdown()
+selected_country = country_dropdown(selected_continent)
 
 if selected_continent != 'Select a continent' and selected_country != 'Select a country':
     st.write(f"You selected {selected_country} in {selected_continent}.")
@@ -27,3 +31,4 @@ elif selected_continent != 'Select a continent':
     st.write(f"You selected {selected_continent}. Please select a country.")
 else:
     st.write("Please select a continent and a country.")
+    
